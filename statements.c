1a1e74b0452e658b9133b4de415391db0a17cd5b
msaarna@gmail.com <msaarna@gmail.com>
2022-05-14 10:49:26 -0400

    multiply by 154 hang fix

--- statements.c
+++ statements.c
@@ -1470,6 +1470,49 @@ void mul(char **statement, int bits)
 	    printf("	asl\n");
 	    if (bits == 16)
 		printf("  rol temp1\n");
+	    printf("	clc\n");
+	    printf("	adc ");
+	    printimmed(statement[4]);
+	    printf("%s\n", statement[4]);
+	    if (bits == 16)
+	    {
+		printf("	tax\n");
+		printf("	lda temp1\n");
+		printf("	adc #0\n");
+		printf("	sta temp1\n");
+		printf("	txa\n");
+	    }
+	    printf("	asl\n");
+	    if (bits == 16)
+		printf("  rol temp1\n");
+	    printf("	clc\n");
+	    printf("	adc ");
+	    printimmed(statement[4]);
+	    printf("%s\n", statement[4]);
+	    if (bits == 16)
+	    {
+		printf("	tax\n");
+		printf("	lda temp1\n");
+		printf("	adc #0\n");
+		printf("	sta temp1\n");
+		printf("	txa\n");
+	    }
+	    tempstorage = 1;
+	}
+	else if (!(multiplicand % 9))
+	{
+	    if (tempstorage)
+	    {
+		strcpy(statement[4], "temp2");
+		printf("	sta temp2\n");
+	    }
+	    multiplicand /= 9;
+	    printf("	asl\n");
+	    if (bits == 16)
+		printf("  rol temp1\n");
+	    printf("	asl\n");
+	    if (bits == 16)
+		printf("  rol temp1\n");
 	    printf("	asl\n");
 	    if (bits == 16)
 		printf("  rol temp1\n");
