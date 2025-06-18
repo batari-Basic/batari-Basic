df24576ae787a99bf73df64c2afdac0243fd38d9
Fred Sauer <fredsa@gmail.com>
2025-06-17 19:28:10 -0700

    Prefix `prerror` messages with `line %d: …`
    When viewing compiler output with a single error message, it's not immediately clear that the number in parentheses is a source file line number as the formatting suggests it's a compiler error number.

--- statements.c
+++ statements.c
@@ -5533,7 +5533,7 @@ void drawscreen()
 
 void prerror(char *myerror)
 {
-    fprintf(stderr, "(%d): %s\n", line, myerror);
+    fprintf(stderr, "line %d: %s\n", line, myerror);
 }
 
 int printimmed(char *value)
